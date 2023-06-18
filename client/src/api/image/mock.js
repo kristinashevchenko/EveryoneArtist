c252649efc5e30c2b61d9ceaf90c79a9847fc31c
Krystsina Shauchenka <krystsina.shauchenka@coverwallet.com>
2023-06-18 17:41:55 +0200

    remove mock

--- client/src/api/image/mock.js
+++ client/src/api/image/mock.js
@@ -6,12 +6,5 @@ export const mockImages = {
     generatedPrompt: 'Cat',
     imageUrl:
       'https://i0.wp.com/hyperallergic-newspack.s3.amazonaws.com/uploads/2021/07/cat_3_1.jpg?w=1200&quality=100&ssl=1'
-  },
-  1: {
-    question: 'Where do you want to draw the palm?',
-    choices: ['City', 'Sea', 'Beach'],
-    answer: 'Palms',
-    imageUrl:
-      'https://cdn.pixabay.com/photo/2023/05/06/20/40/ai-generated-7975133_1280.png'
   }
 };
