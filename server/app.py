fa60635fb908cc4facbc7efc4f4204db241d3ad3
bsommerer <bastian@bs-itservices.de>
2023-07-05 11:22:28 +0200

    Renamed controller.py to app.py -> Start backend now with: python app.py !!! Refactored backend Added comments Resolved prompt generation by ChatGPT Added README for backend Added requirements.txt for backend Changed README for project

--- server/app.py
+++ server/app.py
@@ -1,8 +1,11 @@
 import openai.error
 import requests
 from flask_cors import CORS
-from flask import Flask, request, jsonify, send_file, Response
+from flask import Flask, request, jsonify, Response
 import open_ai_api
+
+# IMPORTANT: Obtain OpenAI API key first at https://platform.openai.com/ and then set following environment variable:
+# OPEN_AI_KEY={YOUR API KEY}
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +13,9 @@ CORS(app)
 
 @app.route("/quiz/answer", methods=['POST'])
 def quiz_answer():
+    """
+    Generate the next question with choices based on the quiz so far.
+    """
     quiz = request.get_json()
     next_element_or_false = open_ai_api.quiz_next_turn(quiz)
 
@@ -21,9 +27,13 @@ def quiz_answer():
 
 @app.route("/quiz/generate", methods=['POST'])
 def quiz_generate():
+    """
+    Generate the prompt and image based on the quiz so far.
+    """
+    use_chatgpt = request.args.get('use_chatgpt', True)
+
     quiz = request.get_json()
-    print(quiz)
-    generated_data = open_ai_api.quiz_generate_image(quiz)
+    generated_data = open_ai_api.quiz_generate_image(quiz, use_chatgpt)
 
     generated_data['imageUrl'] = 'http://localhost:5000/image/' + str(generated_data['imageUrl']).split("//", 1)[-1]
 
@@ -32,6 +42,9 @@ def quiz_generate():
 
 @app.route('/image/<path:custom_path>', methods=['GET'])
 def download_image(custom_path):
+    """
+    Download image and stream it to frontend. Used to deal with problems for image download.
+    """
     image_url = 'https://' + custom_path + '?' + request.query_string.decode()
 
     response = requests.get(image_url, stream=True)
@@ -46,7 +59,18 @@ def download_image(custom_path):
 
 @app.errorhandler(openai.error.OpenAIError)
 def handle_openai_error(e):
+    """
+    Return error to frontend in case of OpenAI API error
+    """
     return f"Error with OpenAI: {e}", 500
+
+
+@app.errorhandler(open_ai_api.ResponseFormatException)
+def handle_openai_error(e):
+    """
+    Return error to frontend in case the response of ChatGPT could not be parsed
+    """
+    return "Response from ChatGPT could not be parsed", 500
 
 
 if __name__ == '__main__':
--- server/open_ai_api.py
+++ server/open_ai_api.py
@@ -2,10 +2,19 @@ import os
 import time
 import openai
 
+# IMPORTANT: Obtain OpenAI API key first at https://platform.openai.com/ and then set following environment variable:
+# OPEN_AI_KEY={YOUR API KEY}
 openai.api_key = os.getenv('OPEN_AI_KEY')
 
 
-def quiz_next_turn(quiz: list[dict]) -> dict | bool:
+def quiz_next_turn(quiz: list[dict]) -> dict:
+    """
+    Generate next conversation element based on previous quiz.
+    :param quiz: The quiz so far
+    :return: The next conversation element of the quiz
+    """
+
+    # Initial prompt for ChatGPT
     messages = [
         {"role": "system",
          "content": "The user wants to draw an image. You help the user by asking what he wants to draw. You "
@@ -18,36 +27,29 @@ def quiz_next_turn(quiz: list[dict]) -> dict | bool:
                     "Always start the conversation by asking which style of an image the user wants to create. "
                     "The user can select multiple answer options if he wants to draw multiple objects."},
         {"role": "system", "name": "example_assistant",
-         "content": "Question?\noption1|option2|option3|option4|option5"}
+         "content": "Question?\nchoice1|choice2|choice3|choice4|choice5"}
     ]
 
-    for elem in quiz:
-        question = str(elem['question']) + "\n" + "|".join(elem["choices"])
-        messages.append({"role": "assistant", "content": question})
+    # Convert current quiz to conversation and append it to initial prompt
+    messages = messages + quiz_to_chatgpt_conversation(quiz)
 
-        if elem.get('answer', False):
-            answer = elem['answer']
-            if type(answer) is list:
-                answer = "&".join(answer)
-            messages.append({"role": "user", "content": str(answer)})
-
+    # Try to generate next conversation element. Try again with additional prompt if ChatGPT fails.
     max_tries = 3
     next_conversation_element = False
+
     for i in range(max_tries):
+        if i != 0:
+            print("Trying conversation: " + str(i + 1) + " of", max_tries)
         try:
             next_conversation_element = get_next_conversation_element(messages)
             break
         except ResponseFormatException:
-            messages.append({"role": "system",
-                             "content": "Stick strictly to following format for your response: "
-                             "Question?\noption1|option2|option3|option4|option5 "
-                             "Do not apologize yourself for this mistake."})
-        except YesNoQuestionException:
-            messages.append({"role": "system",
-                            "content": "Do only ask open questions, without yes or no as answer option. "
-                                       "Do not apologize yourself for this mistake. "
-                                       "Stick strictly to following format for your response: "
-                                       "Question?\noption1|option2|option3|option4|option5"})
+            messages.append({"role": "user",
+                             "content": "Continue asking in following format: "
+                                        "Question?\noption1|option2|option3|option4|option5"})
+
+    if not next_conversation_element:
+        raise ResponseFormatException
 
     return next_conversation_element
 
@@ -56,38 +58,31 @@ class ResponseFormatException(Exception):
     pass
 
 
-class YesNoQuestionException(Exception):
-    pass
+def get_next_conversation_element(messages: list[dict]) -> dict:
+    """
+    Call chatgpt to generate next conversation element. Process the response of ChatGPT and split it into
+    answer options. Raises exceptions if ChatGPT does not answer in the wanted format.
+    :param messages: Messages to send to ChatGPT
+    :return: Next conversation element based on ChatGPTs response
+    """
 
+    # Call ChatGPT to obtain next question and answers in following format:
+    # Question?\nchoice1|choice2|choice3|choice4|choice5
+    response_string = call_chatgpt(messages)
 
-def get_next_conversation_element(messages):
-    response = False
-    for i in range(3):
-        try:
-            response = openai.ChatCompletion.create(
-                model='gpt-3.5-turbo',
-                max_tokens=30,
-                messages=messages
-            )
-            break
-        except openai.error.RateLimitError as e:
-            print(e)
-            time.sleep(18)
+    if not response_string:
+        raise ResponseFormatException
 
-    if not response:
-        return False
-
-    response_string = str(response["choices"][0]["message"]["content"])
-    print("ChatGPT response:", response_string)
-
+    # split question and answer options:
+    # response_list[0]: Question?
+    # response_list[1]: choice1|choice2|choice3|choice4|choice5
     response_list = response_string.split("\n", 2)
 
     if len(response_list) < 2:
         raise ResponseFormatException
 
-    # if "yes" in response_list[1].lower() or "no" in response_list[1].lower():
-    #    raise YesNoQuestionException
-
+    # split single answer choices:
+    # answer_options[i]: choicei
     question = response_list[0]
     answer_options = response_list[1].split("|")
 
@@ -99,6 +94,7 @@ def get_next_conversation_element(messages):
     if len(answer_options) < 2:
         raise ResponseFormatException
 
+    # Build next conversation element out of question and answer options
     next_conversation_element = {
         "question": question,
         "choices": answer_options,
@@ -108,17 +104,29 @@ def get_next_conversation_element(messages):
     return next_conversation_element
 
 
-def quiz_generate_image(quiz: list[dict]) -> dict:
-    prompt = quiz_generate_prompt(quiz)
+def quiz_generate_image(quiz: list[dict], use_chatgpt_for_prompt: bool = True) -> dict:
+    """
+    Generates the image and the prompt for a given quiz.
+    :param quiz: Quiz up to the point at which the image should be generated
+    :param use_chatgpt_for_prompt: Weather to use ChatGPT for generating the prompt or just concatenate the given answers.
+    :return: imageUrl of the generated image with corresponding prompt
+    """
 
+    # 1. Generate prompt
+    if use_chatgpt_for_prompt:
+        prompt = quiz_generate_prompt_with_gpt(quiz)
+    else:
+        prompt = quiz_generate_prompt(quiz)
+
+    # 2. Generate image
     response = openai.Image.create(
         prompt=prompt,
         n=1,
         size="256x256"
     )
-
     image_url = response['data'][0]['url']
 
+    # 3. Generate response
     generation_data = {
         "prompt": prompt,
         "imageUrl": image_url
@@ -128,6 +136,12 @@ def quiz_generate_image(quiz: list[dict]) -> dict:
 
 
 def quiz_generate_prompt(quiz: list[dict]) -> str:
+    """
+    Generate prompt by concatenating the given answers of a quiz into a single string.
+    :param quiz: The quiz to generate the prompt for
+    :return: Prompt for image generation
+    """
+
     answer_list = [elem.get('answer')
                    for elem in quiz
                    if (elem.get('answer') is not None)
@@ -146,3 +160,73 @@ def quiz_generate_prompt(quiz: list[dict]) -> str:
             answer_strings_filtered.append(answer)
 
     return " ".join(answer_strings_filtered)
+
+
+def quiz_generate_prompt_with_gpt(quiz: list[dict]) -> str:
+    """
+    Generate prompt by asking ChatGPT to generate the prompt.
+    :param quiz: The quiz to generate the prompt for
+    :return: Prompt for image generation
+    """
+    messages = quiz_to_chatgpt_conversation(quiz)
+
+    messages.append({"role": "system",
+                     "content": "Create a prompt for an image generator from my given answers. "
+                                "The response should be basic, one sentence and only contain "
+                                "my answers without additional instructions."})
+
+    try:
+        return call_chatgpt(messages)
+    except openai.error.OpenAIError:
+        return quiz_generate_prompt(quiz)
+
+
+def quiz_to_chatgpt_conversation(quiz: list[dict]) -> list[dict]:
+    """
+    Convert the current quiz into a conversation for ChatGPT.
+    :param quiz: The quiz to convert
+    :return: Conversation for ChatGPT
+    """
+    messages = []
+
+    for elem in quiz:
+        question = str(elem['question']) + "\n" + "|".join(elem["choices"])
+        messages.append({"role": "assistant", "content": question})
+
+        if elem.get('answer', False):
+            answer = elem['answer']
+            if type(answer) is list:
+                answer = "&".join(answer)
+            messages.append({"role": "user", "content": str(answer)})
+
+    return messages
+
+
+def call_chatgpt(messages: list[dict]) -> str:
+    """
+    Calls ChatGPT for a response. Raises openai.error.* in case of error.
+    :param messages: List of conversation elements for ChatGPT
+    :return: Response string from ChatGPT
+    """
+    response = False
+    max_tries = 3
+    for i in range(max_tries):
+        if i != 0:
+            print("Calling ChatGPT: " + str(i + 1) + " of", max_tries)
+        try:
+            response = openai.ChatCompletion.create(
+                model='gpt-3.5-turbo',
+                max_tokens=50,
+                messages=messages
+            )
+            break
+        except openai.error.RateLimitError as e:
+            print(e)
+            if i == max_tries - 1:
+                raise e
+            else:
+                time.sleep(20)
+
+    response_string = str(response["choices"][0]["message"]["content"])
+    print("ChatGPT response:", response_string)
+    return response_string
